735e7a464ad55bc0cd0d2029652991ab923776d4
Samuel Woodman <samuel.woodman@gmail.com>
2023-06-12 13:28:28 -0600

    clean formatting

--- code/wetland_coords_to_gpkg.R
+++ code/wetland_coords_to_gpkg.R
@@ -1,12 +1,42 @@
+## ---------------------------
+##
+## Script name: wetland_coords_to_gpkg
+##
+## Purpose of script: Covert lat/lon coordinates provided by Molly to a spatial
+## object. 
+##
+## Author: Samuel Woodman
+##
+## Date Created: 2023-05-02
+##
+## Email: samuel.woodman@gmail.com
+##
+## ---------------------------
+##
+## Notes: 
+## Data were originally from Google Earth and had the ESPG:4326 projection
+##   
+## ---------------------------
+
+options(scipen = 6, digits = 4)
 
 
+# Load packages -----------------------------------------------------------
 
-library(sf)
-library(tidyverse)
+require(tidyverse)
+require(here)
+
 library(readxl)
+library(sf)
 
-pnts <- read_excel("/Users/sam/Downloads/OneDrive_4_4-28-2023/PrairieBCRC_CAAF_sites_27April2023.xls") %>% 
+# Process -----------------------------------------------------------------
+
+## read data and convert to sf object
+pnts <- read_excel(here("data/raw/PrairieBCRC_CAAF_sites_27April2023.xls")) %>% 
   st_as_sf(., coords = c("Longtitude_X", "Latitude_Y"),
            crs = 4326)
 
-st_write(pnts, "/Users/sam/Documents/wetland_flux/test.gpkg", "GPKG")
+
+# Save output -------------------------------------------------------------
+
+st_write(pnts, here("data/raw/wetland_sites.gpkg"), "GPKG")
