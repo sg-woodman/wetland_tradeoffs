528fa1a7f30a7625db7d19d1dd3e02c3cd37e39c
Samuel Woodman <samuel.woodman@gmail.com>
2024-02-01 14:30:47 -0700

    fix to match BCRC data from Paige

--- code/create_skeleton_df.R
+++ code/create_skeleton_df.R
@@ -30,23 +30,26 @@ library(readxl)
 
 # Load data ---------------------------------------------------------------
 
-skeleton_df <- read_xlsx(here("documents/bcrc_site_guide.xlsx")) %>% 
+skeleton_df <- read_xlsx(here("data/raw/BCRC_CAAF_sites_02Jan2024.xlsx")) %>% 
+  # clean names to lowercase
   janitor::clean_names() %>% 
-  separate_wider_delim(
-    cols = site_id,
-    delim = "_", 
-    names = c("province", "region", "site_number")
-  ) %>% 
-  unite("site", c(province, region, site_number), sep = "_", remove = FALSE) %>%
-  mutate(site = str_replace(site, "_Camrose_", "_")) %>% 
-  select(site, province, region, site_number, latitude, longitude, landuse) %>% 
+  # select only columns of use
+  select(objectid, site_id = site_id_current_study_bcrc, province, 
+         latitude_y, longtitude_x, landuse) %>% 
+  # create column of sub-regions and site numbers
+  mutate(region = str_replace_all(site_id, "[:digit:]|AB|SK|MB|ON|BC|\\-", ""), # sub-regions
+         site_no = as.numeric(str_replace_all(site_id, "[:alpha:]|\\-", ""))) %>% # site number
+  # replace empty cells with NA where no region is specified
+  mutate_at("region", ~na_if(., '')) %>% 
+  # create new land classes
   mutate(land_class = case_when(landuse == "Grassland/Cropland" ~ "Both",
                                 str_detect(landuse, "Grassland|Pasture") ~ "Grassland", 
                                  str_detect(landuse, "Cropland") ~ "Cropland" 
                                 ),
-         site = as.factor(site),
+         site = as.factor(site_id),
+         region = as.factor(region),
          province = as.factor(province))
 
 write_csv(skeleton_df, here("data/raw/skeleton_df.csv"))
- s
+
          
