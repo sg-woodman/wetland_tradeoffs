90b44318063c1ec3b69dcc03b67f05e570f68d02
Samuel Woodman <samuel.woodman@gmail.com>
2024-02-01 15:07:58 -0700

    fix regional sites not being properly encoded in site column

--- code/create_skeleton_df.R
+++ code/create_skeleton_df.R
@@ -63,7 +63,8 @@ skeleton_df <- read_xlsx(here("data/raw/BCRC_CAAF_sites_02Jan2024.xlsx")) %>%
               # create column of sub-regions and site numbers
               mutate(region = str_replace_all(site_id, "[:digit:]|AB|SK|MB|ON|BC|\\-", ""), # sub-regions
                      site_no = as.numeric(str_replace_all(site_id, "[:alpha:]|\\-", "")), # site number
-                     site_no = str_pad(site_no, 2, pad = "0")) %>% #  add leading zero to site number
+                     site_no = str_pad(site_no, 2, pad = "0"), #  add leading zero to site number
+                     site = site_id) %>% # add site as column to keep site_id constant
               # replace empty cells with NA where no region is specified
               mutate_at("region", ~na_if(., ''))) %>% 
   # create new land classes
@@ -75,8 +76,6 @@ skeleton_df <- read_xlsx(here("data/raw/BCRC_CAAF_sites_02Jan2024.xlsx")) %>%
          site = as.factor(site),
          region = as.factor(region),
          province = as.factor(province))
-
-
 
 write_csv(skeleton_df, here("data/raw/skeleton_df.csv"))
 
